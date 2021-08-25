97840d887eb99ed306858a7f48482da533b8c244
Hazzzero <58512335+kds0280@users.noreply.github.com>
2021-08-25 14:48:50 +0900

    mooyahoooo

---
+++ app/src/main/java/com/example/greenomy/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.greenomy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private fragment_home fg_home = new fragment_home();
+    private fragment_community fg_community = new fragment_community();
+    private fragment_store fg_store = new fragment_store();
+    private fragment_my fg_my = new fragment_my();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav);
+
+        //첫화면띄우기
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_frame, fg_home).commitAllowingStateLoss();
+
+        bottomNavView.setOnNavigationItemSelectedListener();
+    }
+}
